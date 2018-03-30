84b084af47f028f208cd95f0304ae3a1c154c9ed
Sandra <sandra.schneider@gmx.at>
2018-03-30 17:31:49 +0200

    Add basic project structure

---
+++ Experts/Experts/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Experts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index";
+            return View();
+        }
+
+        public ActionResult CoreData()
+        {
+            ViewBag.Message = "Core data";
+            return View();
+        }
+
+        public ActionResult Achievements()
+        {
+            ViewBag.Message = "Achievements";
+
+            return View();
+        }
+
+        public ActionResult Roles()
+        {
+            ViewBag.Message = "Roles";
+
+            return View();
+        }
+
+        public ActionResult Properties()
+        {
+            ViewBag.Message = "Properties";
+
+            return View();
+        }
+
+    }
+}
---
+++ Experts/Experts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Experts.MvcApplication" Language="C#" %>
