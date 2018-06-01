28eac76b372f58a28f8bf923402d2f15c8269305
Sandra Schneider <se17m030@technikum-wien.at>
2018-06-01 19:44:19 +0200

    Add search and tables via json dummy data and several views

---
+++ Experts/Experts/Content/js/app.js
@@ -0,0 +1,27 @@
+﻿var global = {};
+global.search = {
+    context: null,
+    container: null,
+    init(context, container) {
+        global.search.context = context || '';
+        global.search.container = container;
+        const json = $.getJSON('/Content/json/data' + global.search.context + '.json', function (json) {
+            $('.js-search').select2({
+                data: json.results,
+            });
+
+        });
+
+        $('.js-search').change(function () {
+            global.search.updateTable();
+        });
+    },
+    updateTable() {
+        const selected = $('.js-search').select2('data');
+        $(global.search.container).DataTable().destroy();
+        $(global.search.container).DataTable()
+            .search(selected[0].text);
+        $(global.search.container).DataTable()
+            .draw();
+    }
+}
---
+++ Experts/Experts/Controllers/AchievementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Experts.Controllers
+{
+    public class AchievementController : Controller
+    {
+        // GET: Achievement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
